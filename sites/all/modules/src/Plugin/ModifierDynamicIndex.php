14c345caaa12dcdf9f6104cd7dced4d069922874
Ciprian Oancia <ciprian.oancia+mac_git@ocquia.com>
2017-04-28 15:33:22 +0200

    Patched PURL, Dynamic Provider and Ctools

---
+++ sites/all/modules/src/Plugin/ModifierDynamicIndex.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * 
+ */
+namespace Drupal\domains_rewrites_dynamicaly\Plugin;
+use Drupal\purl\Plugin\ModifierIndex;
+
+class ModifierDynamicIndex extends ModifierIndex{
+
+}
